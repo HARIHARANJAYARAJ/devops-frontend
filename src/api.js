da9f49ff0301f6b22163c7bbadee375899e17cce
Karthikeyan S <hari11132003@gmail.com>
2026-08-31 12:23:28 +0530

    Update frontend backend API URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://15.252.140.18:8000";
+const API_BASE_URL = "http://3.110.171.142:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
